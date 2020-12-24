e0de6fb2808419a01d018a3508a288fc9c5ea4e8
iamsid47 <siddheshkulthe43@gmail.com>
2020-12-24 20:52:20 +0530

    added the python code

---
+++ audiobook.py
@@ -0,0 +1,20 @@
+import pyttsx3
+import pdfplumber
+import PyPDF2
+
+file = 'sample.pdf'
+
+pdfFileObj = open(file, 'rb')
+
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+pages = pdfReader.numPages
+
+with pdfplumber.open(file) as pdf:
+    for i in range(0, pages):
+        page = pdf.pages[i]
+        text = page.extract_text()
+        print(text)
+        speaker = pyttsx3.init()
+        speaker.say(text)
+        speaker.runAndWait()
